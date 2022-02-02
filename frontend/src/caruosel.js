0741e05ec65fbe75e03f1f85d72c8a4e7a29fab2
hekalotanya <hekalot@gmail.com>
2022-02-02 21:11:05 +0200

    all adaptive done

--- frontend/src/caruosel.js
+++ frontend/src/caruosel.js
@@ -5,10 +5,10 @@ const width = 260;
 let position = 0; // положение ленты прокрутки
 
 document.getElementById('arrow_prev').onclick = function() {
-  const blockWidth = document.getElementById('list').offsetWidth;
-  const count = blockWidth / 260;
+  // const blockWidth = document.getElementById('list').offsetWidth;
+  // const count = blockWidth / 260;
 
-  position += width * count;
+  position += width;
   position = Math.min(position, 0);
   list.style.marginLeft = position + 'px';
 };
@@ -17,7 +17,7 @@ document.getElementById('arrow_next').onclick = function() {
   const blockWidth = document.getElementById('list').offsetWidth;
   const count = blockWidth / 260;
 
-  position -= width * count;
+  position -= width;
   position = Math.max(position, -width * (listElems.length - count));
   list.style.marginLeft = position + 'px';
 };
