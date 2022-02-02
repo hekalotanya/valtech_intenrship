70297fe59a5b5aeb4e68270a6adad2ed4afbfdff
hekalotanya <hekalot@gmail.com>
2022-02-02 17:30:10 +0200

    mobile adaptive was added

---
+++ frontend/src/caruosel.js
@@ -0,0 +1,23 @@
+const list = document.getElementById('caruosel');
+const listElems = list.querySelectorAll('li');
+const width = 260;
+
+let position = 0; // положение ленты прокрутки
+
+document.getElementById('arrow_prev').onclick = function() {
+  const blockWidth = document.getElementById('list').offsetWidth;
+  const count = blockWidth / 260;
+
+  position += width * count;
+  position = Math.min(position, 0);
+  list.style.marginLeft = position + 'px';
+};
+
+document.getElementById('arrow_next').onclick = function() {
+  const blockWidth = document.getElementById('list').offsetWidth;
+  const count = blockWidth / 260;
+
+  position -= width * count;
+  position = Math.max(position, -width * (listElems.length - count));
+  list.style.marginLeft = position + 'px';
+};
